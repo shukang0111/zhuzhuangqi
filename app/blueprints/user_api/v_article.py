d0c36be29adb8901f65b1c135206d421448d17ec
shukang0111 <13720144949@163.com>
2020-11-26 14:31:08 +0800

    公众号文章列表更新

--- app/blueprints/user_api/v_article.py
+++ app/blueprints/user_api/v_article.py
@@ -1,19 +1,22 @@
 from . import bp_user_api
 from ...api_utils import *
-from ...models import Article
+from ...models import Article, ArticleType
 
 
-@bp_user_api.route('/article/list/', methods=['GET'])
-def get_user_article_list():
+@bp_user_api.route('/article/list/<int:article_type_id>/', methods=['GET'])
+def get_user_article_list(article_type_id):
     """公众号文章列表"""
-    query = Article.select().where(Article.is_delete == 0, Article.show == 1).order_by(Article.weight.desc())
-    _article = list()
+    article_type = ArticleType.get_by_id(article_type_id, code=1104)
+    item = article_type.to_dict()
+    query = Article.select().where(Article.article_type_id == article_type_id, Article.is_delete == 0)
+    _articles = list()
     for article in query:
         item = article.to_dict()
         item['total_use_count'] = item['real_use_count'] + item['extra_add_count']
-        _article.append(item)
+        _articles.append(item)
+    item['article_list'] = _articles
     data = {
-        "articles": _article
+        "articles": _articles
     }
     return api_success_response(data)
 

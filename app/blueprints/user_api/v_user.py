d550669a3007f0b4118471a76a8b870f4cb1c64e
shukang0111 <13720144949@163.com>
2020-11-30 16:37:23 +0800

    微信分享config url调试

--- app/blueprints/user_api/v_user.py
+++ app/blueprints/user_api/v_user.py
@@ -86,10 +86,8 @@ def get_open_upload_file_tokens():
 def get_weixin_ticket():
     """前端获取js配置信息"""
     # url = request.args.get('url')
-    # zzq_url = "https://zzqapi.e-shigong.com"
-    # url = request.full_path
-    url = "https://{}{}".format(request.host, request.uri)
-    current_app.logger.info(url)
+    zzq_url = "https://zzqapi.e-shigong.com"
+    url = request.full_path
     wx_user = g.wx_user
 
     weixin_sign = get_weixin_sign(url)

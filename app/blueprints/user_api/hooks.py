11d062d6c052df620a3d8163631f1debf191b978
shukang0111 <13720144949@163.com>
2020-12-01 14:58:40 +0800

    点击分享链接

--- app/blueprints/user_api/hooks.py
+++ app/blueprints/user_api/hooks.py
@@ -13,8 +13,8 @@ def user_authentication():
     :return:
     """
     g.wx_user = None
-    # if request.endpoint.split('.')[-1] in ["get_open_upload_file_tokens", "index"]:
-    #     return
+    if request.endpoint.split('.')[-1] in ["get_open_upload_file_tokens", "index"]:
+        return
     # current_app.logger.info('{}-{}'.format(request.headers, request.full_path))
     # wx_user = WXUser.select().where(WXUser.id.in_([23, 24])).get()
     # g.wx_user = wx_user

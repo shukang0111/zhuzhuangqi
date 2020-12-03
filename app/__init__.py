aa992938e91d1f23703a654a58abac8f798c0b25
shukang0111 <13720144949@163.com>
2020-12-03 20:06:00 +0800

    跨域解决尝试1

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, Response
+from werkzeug.datastructures import Headers
 from flask_cors import *
 from app.config import Config
+
+
+class MyResponse(Response):
+    def __init__(self, response=None, **kwargs):
+        kwargs['headers'] = ''
+        headers = kwargs.get('headers')
+        # 跨域控制
+        origin = ('Access-Control-Allow-Origin', '*')
+        methods = ('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, DELETE, PUT')
+        if headers:
+            headers.add(*origin)
+            headers.add(*methods)
+        else:
+            headers = Headers([origin, methods])
+        kwargs['headers'] = headers
+        return super().__init__(response, **kwargs)
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app, supports_credentials=True)
     app.config.from_object(Config)
+    app.response_class = MyResponse
     Config.init_app(app)
     return app

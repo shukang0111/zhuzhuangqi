ce5c37fb7bece92685848b31c84f287c12efc62f
shukang0111 <13720144949@163.com>
2020-12-04 19:07:38 +0800

    前端授权

--- app/blueprints/user_api/hooks.py
+++ app/blueprints/user_api/hooks.py
@@ -19,23 +19,23 @@ def user_authentication():
     # # wx_user = WXUser.select().where(WXUser.id.in_([23, 24])).get()
     # # g.wx_user = wx_user
     # # return
-    token = request.headers.get('Authorization')
-    if token:
-        wx_user = WXUser.get_by_token(token)
-        if wx_user:
-            g.wx_user = wx_user  # g.admin
-            return
-    abort(401)
-    # openid = session.get("openid")
-    # current_app.logger.info(openid)
-    # code = request.args.get("code")
-    # redirect_url = request.url
-    # current_app.logger.info("{0}_{1}".format(code, redirect_url))
-    # current_app.logger.info(request.referrer)
-    # if not openid:
-    #     if not code:
-    #         return redirect(get_auth_url(redirect_url))
-    #     else:
-    #         pass
-    #
-    # return
+    # token = request.headers.get('Authorization')
+    # if token:
+    #     wx_user = WXUser.get_by_token(token)
+    #     if wx_user:
+    #         g.wx_user = wx_user  # g.admin
+    #         return
+    # abort(401)
+    openid = session.get("openid")
+    current_app.logger.info(openid)
+    code = request.args.get("code")
+    redirect_url = request.url
+    current_app.logger.info("{0}_{1}".format(code, redirect_url))
+    current_app.logger.info(request.referrer)
+    if not openid:
+        if not code:
+            return redirect(get_auth_url(redirect_url))
+        else:
+            pass
+
+    return

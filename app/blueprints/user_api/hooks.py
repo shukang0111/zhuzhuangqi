7a130c9e5a5f8ff6f9b31be0472a33ab75613c25
shukang0111 <13720144949@163.com>
2020-12-22 11:29:11 +0800

    调试

--- app/blueprints/user_api/hooks.py
+++ app/blueprints/user_api/hooks.py
@@ -15,15 +15,15 @@ def user_authentication():
     # g.wx_user = None
     if request.endpoint.split('.')[-1] in ["get_open_upload_file_tokens", 'index']:
         return
-    # current_app.logger.info('{}-{}'.format(request.headers, request.full_path))
-    # wx_user = WXUser.select().where(WXUser.id == 5).get()
-    # g.wx_user = wx_user
-    # return
-    token = request.headers.get('Authorization')
-    if token:
-        wx_user = WXUser.get_by_token(token)
-        if wx_user:
-            g.wx_user = wx_user  # g.admin
-            return
-    abort(401)
+    current_app.logger.info('{}-{}'.format(request.headers, request.full_path))
+    wx_user = WXUser.select().where(WXUser.id.in_([23, 24])).get()
+    g.wx_user = wx_user
+    return
+    # token = request.headers.get('Authorization')
+    # if token:
+    #     wx_user = WXUser.get_by_token(token)
+    #     if wx_user:
+    #         g.wx_user = wx_user  # g.admin
+    #         return
+    # abort(401)
 

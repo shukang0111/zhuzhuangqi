2cac34e3e8c0e140b496590589e44be7acd1828e
shukang0111 <13720144949@163.com>
2021-03-31 16:01:07 +0800

    bug fix

--- app/blueprints/user_api/v_user.py
+++ app/blueprints/user_api/v_user.py
@@ -95,8 +95,9 @@ def get_weixin_ticket():
     """前端获取js配置信息"""
     wx_user = g.wx_user
     full_path = request.full_path
+    current_app.logger.info(full_path)
     wx_url = full_path.split('?')[1] + '?' + full_path.split('?')[2]
-    current_app.logger.info('wx_url')
+    current_app.logger.info(wx_url)
     weixin_sign = get_weixin_sign(wx_url)
     weixin_sign['oid'] = wx_user.openid
     data = {
